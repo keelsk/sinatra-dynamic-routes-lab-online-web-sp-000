fe5d8e22a369ba9c760bb34a327d906f06c84c0f
Kirsten Keels <keelsk@gmail.com>
2020-03-27 22:23:08 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -15,5 +15,10 @@ class App < Sinatra::Base
   get '/say/:number/:phrase' do 
     erb :say
   end
+  
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
+    @word1 = params[:word1]
+    ""
+  end
 
 end
