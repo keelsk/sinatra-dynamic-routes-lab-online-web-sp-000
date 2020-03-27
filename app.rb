3dc7a79203d3f9ee01e1e6cc6a2db200fec62b4d
Kirsten Keels <keelsk@gmail.com>
2020-03-27 22:05:55 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,5 +2,21 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   # Write your code here!
+  get '/reversename/:name' do
+    @name = params[:name].reverse
+    "#{@name}"
+  end
+  
+  get '/square/:number' do 
+    @square = params[:number].to_i * params[:number].to_i
+    "#{@square}"
+  end
+  
+  get '/say/:number/:phrase' do 
+    @number = params[:number].to_i
+ 
+      "kirsten"
+
+  end
 
 end
